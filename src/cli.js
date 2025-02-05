7d4eeffc7bbf228b5fb8418b18bf834437481892
dven-dev <me@dven.dev>
2025-02-05 15:11:42 +0300

    fix linter errors

--- src/cli.js
+++ src/cli.js
@@ -4,5 +4,4 @@ export const greeting = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  return name;
 };
