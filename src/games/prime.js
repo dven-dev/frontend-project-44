25d4cd417f4b730f44c3c01fed8e3efc62e7bb35
dven-dev <me@dven.dev>
2025-02-05 15:07:29 +0300

    fix linter errors

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,17 +1,12 @@
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  if (num < 2) {
-  return false;
-  }
+  if (num < 2) { return false; }
   for (let i = 2; i <= Math.sqrt(num); i += 1) {
-    if (num % i === 0) {
-    return false;
-    }
+    if (num % i === 0) { return false; }
   }
   return true;
 };
-
 
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
